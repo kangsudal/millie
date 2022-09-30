f0153f1ed82aa56f674e07a6653b00358601ac4e
kangsudal_notebook <kangsudal@gmail.com>
2022-09-30 12:49:18 +0900

    Simply create a CarouselSlider widget

--- lib/screens/0_today/today.dart
+++ lib/screens/0_today/today.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class TodayScreen extends StatefulWidget {
@@ -15,6 +17,23 @@ class _TodayScreenState extends State<TodayScreen> {
       SingleChildScrollView(
         child: Column(
           children: [
+            CarouselSlider(
+              options: CarouselOptions(height: 300.0),
+              items: [1,2,3,4,5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                            color: Colors.amber
+                        ),
+                        child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                    );
+                  },
+                );
+              }).toList(),
+            ),
             Container(
               height: 300,
               color: Colors.pink,
