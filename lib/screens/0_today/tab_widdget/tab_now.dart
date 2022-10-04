afca2f9e0b54dae642d51a75c1d3a6484fb16464
kangsudal_notebook <kangsudal@gmail.com>
2022-10-04 15:53:38 +0900

    floating images

--- lib/screens/0_today/tab_widdget/tab_now.dart
+++ lib/screens/0_today/tab_widdget/tab_now.dart
@@ -70,25 +70,84 @@ class NewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
+    final dataList = [
+      {
+        'title': '책 추천 맛집 밀리, \n6명의 큐레이터 공개',
+        'subTitle': '무슨 책을 읽을지 고민이라면 주목!',
+        'img': 'images/library.png',
+        // 'https://images.unsplash.com/photo-1497354861845-d381fb7c91a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      },
+      {
+        'title': '둘 중 어느영상이 AI작품을까요?',
+        'subTitle': 'AI가 영상을 만들 수 있다고?',
+        'img': 'images/robot.png',
+        // 'https://images.unsplash.com/photo-1539606328118-80c679838702?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80',
+      },
+      {
+        'title': '소중한 저작권 \n함께 지켜요!',
+        'subTitle': '이벤트 참여하고 바나나 우유받기',
+        'img': 'images/banana.png',
+        // 'https://images.unsplash.com/photo-1528825871115-3581a5387919?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80',
+      }
+    ];
+    return CarouselSlider.builder(
       options: CarouselOptions(
+        viewportFraction: 1,
         height: 300.0,
         autoPlay: true,
       ),
-      items: [1, 2, 3, 4, 5].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                decoration: BoxDecoration(color: Colors.amber),
-                child: Text(
-                  'text $i',
-                  style: TextStyle(fontSize: 16.0),
-                ));
-          },
+      itemCount: 3,
+      itemBuilder: (BuildContext context, int index, int realIndex) {
+        final String title = dataList[index]['title'] ?? 'title';
+        final subTitle = dataList[index]['subTitle'] ?? 'subTitle';
+        final img = dataList[index]['img'] ?? 'images/bear.png';
+            // 'https://images.unsplash.com/photo-1497354861845-d381fb7c91a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+
+        return Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  height: 250,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.primaries[index].withOpacity(0.5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 30),
+                        ),
+                        Text(
+                          subTitle,
+                          style: TextStyle(color: Colors.black38, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                ), //white empty area
+              ],
+            ),
+            Positioned(
+              child: SizedBox(
+                child: Image.asset(img,fit: BoxFit.contain,),
+                width: 150,
+                height: 180,
+              ),
+              bottom: 0,
+              right: 20,
+            ),
+          ],
         );
-      }).toList(),
+      },
     );
   }
 }
